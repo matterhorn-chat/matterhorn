81d560114d0f71d6e79b26b76ed9e7f24d200b34
Getty Ritter <gdritter@galois.com>
2016-09-07 23:18:07 -0700

    Using inherent User ordering attribute

--- src/Types.hs
+++ src/Types.hs
@@ -191,6 +191,15 @@ data UserInfo = UserInfo
 
 makeLenses ''UserInfo
 
+instance Ord UserInfo where
+  u1 `compare` u2
+    | u1^.uiStatus /= Offline && u2^.uiStatus == Offline
+      = LT
+    | u1^.uiStatus == Offline && u2^.uiStatus /= Offline
+      = GT
+    | otherwise
+      = (u1^.uiName) `compare` (u2^.uiName)
+
 userInfoFromProfile :: UserProfile -> UserInfo
 userInfoFromProfile up = UserInfo
   { _uiName   = userProfileUsername up
