842e85046c2f4a45146be0b5769db47aaa81a8b8
Jonathan Daugherty <cygnus@foobox.com>
2016-11-19 12:24:56 -0800

    Whitespace

--- src/Draw/Main.hs
+++ src/Draw/Main.hs
@@ -249,12 +249,14 @@ renderCurrentChannelDisplay uSet st = (header <+> conn) <=> messages
                  False -> renderText $
                           mkChannelName (chan^.ccInfo) <> " - " <> topicStr
     messages = body <+> txt " "
+
     body = chatText <=> case chan^.ccInfo.cdCurrentState of
       ChanUnloaded   -> withDefAttr clientMessageAttr $
                           txt "[Loading channel scrollback...]"
       ChanRefreshing -> withDefAttr clientMessageAttr $
                           txt "[Refreshing scrollback...]"
       _              -> emptyWidget
+
     chatText = case st^.csMode of
         ChannelScroll ->
             viewport (ChannelMessages cId) Vertical $
