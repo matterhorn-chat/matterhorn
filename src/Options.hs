9dc6c92890d953f20a876a0e48852ec91c6580a1
Getty Ritter <gdritter@galois.com>
2016-11-22 15:25:33 -0800

    Added command-line flags for help and version information

--- src/Options.hs
+++ src/Options.hs
@@ -1,18 +1,31 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Options where
 
+import Data.Version (showVersion)
+import Development.GitRev
+import Paths_matterhorn (version)
 import System.Console.GetOpt
 import System.Environment (getArgs)
-import System.Exit (exitFailure)
+import System.Exit (exitFailure, exitSuccess)
+
+data Behaviour
+  = Normal
+  | ShowVersion
+  | ShowHelp
+    deriving (Eq, Show)
 
 data Options = Options
   { optConfLocation :: Maybe FilePath
   , optLogLocation  :: Maybe FilePath
+  , optBehaviour    :: Behaviour
   } deriving (Eq, Show)
 
 defaultOptions :: Options
 defaultOptions = Options
   { optConfLocation = Nothing
   , optLogLocation  = Nothing
+  , optBehaviour    = Normal
   }
 
 optDescrs :: [OptDescr (Options -> Options)]
@@ -23,7 +36,19 @@ optDescrs =
   , Option ['l'] ["logs"]
     (ReqArg (\ path c -> c { optLogLocation = Just path }) "PATH")
     "Path to the desired debug logs"
+  , Option ['v'] ["version"]
+    (NoArg (\ c -> c { optBehaviour = ShowVersion }))
+    "Print version information and exit"
+  , Option ['h'] ["help"]
+    (NoArg (\ c -> c { optBehaviour = ShowHelp }))
+    "Print help for command-line flags and exit"
   ]
+
+mhVersion :: String
+mhVersion
+  | $(gitHash) == ("UNKNOWN" :: String) = "matterhorn " ++ showVersion version
+  | otherwise = "matterhorn " ++ showVersion version ++ " (" ++
+                $(gitBranch) ++ "@" ++ $(gitHash) ++ ")"
 
 usage :: IO ()
 usage = putStr (usageInfo "matterhorn" optDescrs)
@@ -32,8 +57,12 @@ grabOptions :: IO Options
 grabOptions = do
   args <- getArgs
   case getOpt Permute optDescrs args of
-    (aps, [], []) ->
-      return (foldr (.) id aps defaultOptions)
+    (aps, [], []) -> do
+      let rs = foldr (.) id aps defaultOptions
+      case optBehaviour rs of
+        Normal -> return rs
+        ShowHelp -> usage >> exitSuccess
+        ShowVersion -> putStrLn mhVersion >> exitSuccess
     (_, _, []) -> do
       usage
       exitFailure
