fc58af7c38eba7302e87ef08200ec55c65bcaaf1
Jonathan Daugherty <cygnus@foobox.com>
2017-02-03 23:02:14 -0800

    Whitespace

--- src/Draw/Main.hs
+++ src/Draw/Main.hs
@@ -466,10 +466,10 @@ messageSelectBottomBar st =
                                 else Nothing
         isMine msg = (Just $ st^.csMe.userUsernameL) == msg^.mUserName
         hasURLs = not . Seq.null . msgURLs
-        options = [ (const True,   "r",       "reply")
-                  , (isMine,       "e",       "edit")
-                  , (isMine,       "d",       "delete")
-                  , (hasURLs,      "o",       "open URL(s)")
+        options = [ (const True, "r", "reply")
+                  , (isMine,     "e", "edit")
+                  , (isMine,     "d", "delete")
+                  , (hasURLs,    "o", "open URL(s)")
                   ]
         Just selPostId = selectMessagePostId $ st^.csMessageSelect
         cid = st^.csCurrentChannelId
