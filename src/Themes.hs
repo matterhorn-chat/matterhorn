f3c2bb44e2f1bd320683baf3fae56175d2692b97
Jonathan Daugherty <cygnus@foobox.com>
2017-03-19 18:40:38 -0700

    Whitespace

--- src/Themes.hs
+++ src/Themes.hs
@@ -241,7 +241,7 @@ attrForUsername s
       "-" `T.isPrefixOf` s ||
       " " `T.isPrefixOf` s
       = usernameAttr $ hash (T.tail s) `mod` (length usernameColors)
-    | otherwise            = usernameAttr $ hash s          `mod` (length usernameColors)
+    | otherwise = usernameAttr $ hash s `mod` (length usernameColors)
 
 usernameColors :: [Attr]
 usernameColors =
