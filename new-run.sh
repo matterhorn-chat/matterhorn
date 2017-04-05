75d03a4d5d1a92e9b054c48ab3ff72bd650adfd1
Jonathan Daugherty <cygnus@foobox.com>
2017-04-05 14:50:02 -0700

    new-run.sh: whitespace

--- new-run.sh
+++ new-run.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# Before you ask: yes, I know about -executable, but it only works for GNU find.
-# Using -perm +111 should work solidly across platforms.
+# Before you ask: yes, I know about -executable, but it only works for
+# GNU find. Using -perm +111 should work solidly across platforms.
 OPTIONS=$(find . -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
 SORTED=$(ls -t ${OPTIONS})
 
-# This is not elegant but I believe it's portable across shells. If there's a
-# more elegant way to do it, please patch.
+# This is not elegant but I believe it's portable across shells. If
+# there's a more elegant way to do it, please patch.
 for OPTION in ${SORTED}; do
   exec "${OPTION}" ${1+$@}
 done
