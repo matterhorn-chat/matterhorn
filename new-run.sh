7b42d57fc9baf11d6d6c2ff669b3bc70a04b2efd
Jason Dagit <dagit@galois.com>
2017-04-05 13:49:45 -0700

    new-run.sh: attempt a more portable find invocation

--- new-run.sh
+++ new-run.sh
@@ -4,7 +4,7 @@ set -e
 
 # Before you ask: yes, I know about -executable, but it only works for GNU find.
 # Using -perm +111 should work solidly across platforms.
-OPTIONS=$(find . -name matterhorn -type f -perm +111)
+OPTIONS=$(find . -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
 SORTED=$(ls -t ${OPTIONS})
 
 # This is not elegant but I believe it's portable across shells. If there's a
