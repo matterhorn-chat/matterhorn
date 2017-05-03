014e102cac408f84cf408395d9984b2167a5cfcc
Getty Ritter <gdritter@galois.com>
2017-05-03 11:19:47 -0700

    Replaced scroll mode event handling with keybinding list

--- src/Events/ChannelScroll.hs
+++ src/Events/ChannelScroll.hs
@@ -10,16 +10,24 @@ import Lens.Micro.Platform
 import Types
 import State
 
+channelScrollKeybindings :: [Keybinding]
+channelScrollKeybindings =
+  [ KB "Load more messages in the current channel"
+    (Vty.EvKey (Vty.KChar 'b') [Vty.MCtrl])
+    loadMoreMessages
+  , KB "Scroll up" (Vty.EvKey Vty.KPageUp [])
+    channelPageUp
+  , KB "Scroll down" (Vty.EvKey Vty.KPageDown [])
+    channelPageDown
+  , KB "Cancel scrolling and return to channel view"
+    (Vty.EvKey Vty.KEsc []) $
+    csMode .= Main
+  ]
+
 onEventChannelScroll :: Vty.Event -> MH ()
 onEventChannelScroll (Vty.EvResize _ _) =
     mh invalidateCache
-onEventChannelScroll (Vty.EvKey Vty.KPageUp []) =
-    channelPageUp
-onEventChannelScroll (Vty.EvKey (Vty.KChar 'b') [Vty.MCtrl]) =
-    loadMoreMessages
-onEventChannelScroll (Vty.EvKey Vty.KPageDown []) =
-    channelPageDown
-onEventChannelScroll (Vty.EvKey Vty.KEsc []) = do
-    csMode .= Main
+onEventChannelScroll e
+  | Just kb <- lookupKeybinding e channelScrollKeybindings = kbAction kb
 onEventChannelScroll _ = do
     return ()
