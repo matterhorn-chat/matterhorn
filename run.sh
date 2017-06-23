ddbf5a689b82b6cb5fb38e55ad361cf747357e26
Jonathan Daugherty <cygnus@foobox.com>
2017-06-23 10:21:01 -0700

    run.sh: comment editing and update to ignore CWD

--- run.sh
+++ run.sh
@@ -7,13 +7,15 @@
 
 set -e
 
-# Before you ask: yes, I know about -executable, but it only works for
-# GNU find. Using -perm +111 should work solidly across platforms.
-OPTIONS=$(find . -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
+HERE=$(cd `dirname $0`; pwd)
+
+# Portability note: -executable is only compatible with GNU find but
+# this invocation should be more portable.
+OPTIONS=$(find $HERE -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
 SORTED=$(ls -t ${OPTIONS})
 
-# This is not elegant but I believe it's portable across shells. If
-# there's a more elegant way to do it, please patch.
+# Run the most recently-modified binary that we found. Note that since
+# we use exec, this loop never makes it past one iteration.
 for OPTION in ${SORTED}; do
   exec "${OPTION}" ${1+$@}
 done
