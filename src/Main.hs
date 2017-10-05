2f49c7141cd7c25319fccbaa662ef468ed082f58
Jonathan Daugherty <cygnus@foobox.com>
2017-10-05 15:34:54 -0700

    Factor out core application setup from Main to App

--- src/Main.hs
+++ src/Main.hs
@@ -5,70 +5,26 @@ module Main where
 import           Prelude ()
 import           Prelude.Compat
 
-import           Brick
-import           Brick.BChan
-import qualified Control.Concurrent.STM as STM
 import           Data.Monoid ((<>))
-import qualified Graphics.Vty as Vty
 import           Lens.Micro.Platform
 import           System.Exit (exitFailure)
-import           System.IO (IOMode(WriteMode), openFile, hClose)
-import           Text.Aspell (stopAspell)
 
 import           Config
 import           Options
-import           State.Setup
-import           Events
-import           Draw
 import           Types
 import           InputHistory
-import           AsyncThread
+import           App
 
 main :: IO ()
 main = do
-  opts <- grabOptions
-  configResult <- findConfig (optConfLocation opts)
-  config <- case configResult of
-      Left err -> do
-          putStrLn $ "Error loading config: " <> err
-          exitFailure
-      Right c -> return c
+    opts <- grabOptions
+    configResult <- findConfig (optConfLocation opts)
+    config <- case configResult of
+        Left err -> do
+            putStrLn $ "Error loading config: " <> err
+            exitFailure
+        Right c -> return c
 
-  eventChan <- newBChan 25
-  writeBChan eventChan RefreshWebsocketEvent
+    finalSt <- runMatterhorn opts config
 
-  requestChan <- STM.atomically STM.newTChan
-
-  startAsyncWorkerThread config requestChan eventChan
-
-  logFile <- case optLogLocation opts of
-    Just path -> Just `fmap` openFile path WriteMode
-    Nothing   -> return Nothing
-
-  st <- setupState logFile config requestChan eventChan
-
-  let mkVty = do
-        vty <- Vty.mkVty Vty.defaultConfig
-        let output = Vty.outputIface vty
-        Vty.setMode output Vty.BracketedPaste True
-        return vty
-
-  finalSt <- customMain mkVty (Just eventChan) app st
-
-  case finalSt^.csEditState.cedSpellChecker of
-      Nothing -> return ()
-      Just (s, _) -> stopAspell s
-
-  case logFile of
-    Nothing -> return ()
-    Just h -> hClose h
-  writeHistory (finalSt^.csEditState.cedInputHistory)
-
-app :: App ChatState MHEvent Name
-app = App
-  { appDraw         = draw
-  , appChooseCursor = showFirstCursor
-  , appHandleEvent  = onEvent
-  , appStartEvent   = return
-  , appAttrMap      = (^.csResources.crTheme)
-  }
+    writeHistory (finalSt^.csEditState.cedInputHistory)
