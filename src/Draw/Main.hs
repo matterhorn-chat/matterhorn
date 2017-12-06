5496599e60288dfa3b3e21e161c7345f7a6fc8f8
Jonathan Daugherty <cygnus@foobox.com>
2017-12-05 16:17:37 -0800

    Draw: convert channel header newlines to spaces rather than removing

--- src/Draw/Main.hs
+++ src/Draw/Main.hs
@@ -297,7 +297,10 @@ renderChannelHeader uSet cSet st chan =
                     Nothing -> mkChannelName (chan^.ccInfo)
                     Just u  -> userHeader u
             _ -> mkChannelName (chan^.ccInfo)
-    in renderText' uSet cSet (T.filter (not . (== '\n')) (channelNameString <> maybeTopic))
+        newlineToSpace '\n' = ' '
+        newlineToSpace c = c
+
+    in renderText' uSet cSet (T.map newlineToSpace (channelNameString <> maybeTopic))
 
 renderCurrentChannelDisplay :: UserSet -> ChannelSet -> ChatState -> Widget Name
 renderCurrentChannelDisplay uSet cSet st = (header <+> conn) <=> messages
