b21a7f466764ed754c8f2dfe198988969c05665f
Kevin Quick <kquick@galois.com>
2017-12-12 23:09:06 -0800

    Revert changes to install.sh generation of cabal.project.local.

--- install.sh
+++ install.sh
@@ -30,12 +30,9 @@ function init {
     if [ ! -f "$HERE/cabal.project.local" ]
     then
         FIRST_TIME=1
-        echo "packages: ${MATTERMOST_API_DIR}/mattermost-api.cabal"  >cabal.project.local
-        echo "          ${MATTERMOST_API_QC_DIR}/mattermost-api-qc.cabal" >>cabal.project.local
-        echo "          ${ASPELL_PIPE_DIR}/aspell-pipe.cabal" >>cabal.project.local
-        echo "Built cabal.project.local:"
-        cat cabal.project.local
-        echo "----"
+        echo 'packages: deps/mattermost-api/mattermost-api.cabal'  >cabal.project.local
+        echo '          deps/mattermost-api-qc/mattermost-api-qc.cabal' >>cabal.project.local
+        echo '          deps/aspell-pipe/aspell-pipe.cabal' >>cabal.project.local
     fi
 }
 
