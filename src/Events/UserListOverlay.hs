06dc9a6d788f33a56bd7dbdf71dac9c5b031cfdb
Jonathan Daugherty <cygnus@foobox.com>
2018-02-02 18:32:18 -0800

    Comment update

--- src/Events/UserListOverlay.hs
+++ src/Events/UserListOverlay.hs
@@ -10,7 +10,7 @@ onEventUserListOverlay :: Vty.Event -> MH ()
 onEventUserListOverlay =
   handleKeyboardEvent userListOverlayKeybindings $ \ _ -> return ()
 
--- | The keybindings we want to use while viewing a post list overlay
+-- | The keybindings we want to use while viewing a user list overlay
 userListOverlayKeybindings :: KeyConfig -> [Keybinding]
 userListOverlayKeybindings = mkKeybindings
   [ mkKb CancelEvent "Exit user browsing" exitUserListMode
