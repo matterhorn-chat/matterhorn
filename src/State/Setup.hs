b0c3f8cde4a4f8afa9194677008d222505523282
Jonathan Daugherty <cygnus@foobox.com>
2018-03-02 15:46:50 -0800

    Setup: reduce connection pool idle connection timeout setting

--- src/State/Setup.hs
+++ src/State/Setup.hs
@@ -62,7 +62,7 @@ setupState logFile initialConfig = do
         Nothing -> id
         Just f  -> \ cd -> cd `withLogger` mmLoggerDebug f
 
-      poolCfg = ConnectionPoolConfig { cpIdleConnTimeout = 60
+      poolCfg = ConnectionPoolConfig { cpIdleConnTimeout = 10
                                      , cpStripesCount = 1
                                      , cpMaxConnCount = 5
                                      }
