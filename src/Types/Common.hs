134e0d542abca1e8edc1d4d4e4b0a1c2c6b07852
Jonathan Daugherty <cygnus@foobox.com>
2018-04-26 14:39:31 -0700

    Types.Common: add sanitation functions for individual characters and text input

--- src/Types/Common.hs
+++ src/Types/Common.hs
@@ -1,5 +1,7 @@
 module Types.Common
   ( sanitizeUserText
+  , sanitizeUserText'
+  , sanitizeChar
   )
 where
 
@@ -11,7 +13,14 @@ import qualified Data.Text as T
 import Network.Mattermost.Types ( UserText, unsafeUserText )
 
 sanitizeUserText :: UserText -> T.Text
-sanitizeUserText ut =
+sanitizeUserText = sanitizeUserText' . unsafeUserText
+
+sanitizeUserText' :: T.Text -> T.Text
+sanitizeUserText' t =
     T.replace "\ESC" "<ESC>" $
-    T.replace "\t" " " $
-    unsafeUserText ut
+    T.replace "\t" " " t
+
+sanitizeChar :: Char -> T.Text
+sanitizeChar '\ESC' = "<ESC>"
+sanitizeChar '\t' = " "
+sanitizeChar c = T.singleton c
