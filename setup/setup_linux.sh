d78f0ee66718f7998431e33ffda40df6f747cfc3
Jonathan Daugherty <cygnus@foobox.com>
2018-12-04 15:24:35 -0800

    Add GHC and cabal build steps to setup_linux for CentOS

--- setup/setup_linux.sh
+++ setup/setup_linux.sh
@@ -33,8 +33,28 @@ function install_packages {
             zlib-devel
     elif centos
     then
-        sudo yum install -y gmp-devel gmp zlib-devel ncurses-devel
+        sudo yum install -y gmp-devel gmp zlib-devel ncurses-devel wget
         sudo yum groupinstall -y "Development Tools"
+
+        # Yes, Debian 8 GHC builds are used for CentOS.
+        GHC_VER=8.4.4
+        GHC_DIR=ghc-$GHC_VER
+        GHC_PKG=$GHC_DIR-x86_64-deb8-linux.tar.xz
+
+        CABAL_VER=2.4.1.0
+        CABAL_DIR=cabal-install-$CABAL_VER
+        CABAL_PKG=$CABAL_DIR.tar.gz
+
+        wget -C https://downloads.haskell.org/~ghc/$GHC_VER/$GHC_PKG
+        wget -C http://hackage.haskell.org/package/$CABAL_DIR/$CABAL_PKG
+
+        tar -xf $GHC_PKG
+        cd $GHC_DIR && ./configure --prefix=/usr/local/ && make install && cd ..
+
+        tar -xf $CABAL_PKG
+        cd $CABAL_DIR && ./bootstrap.sh && cd ..
+
+        echo "Setup done. NOTE! Add $HOME/.cabal/bin/ to your PATH."
     else
         echo "Unsupported Linux distribution"
         exit 1
