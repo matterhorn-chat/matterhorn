d42c95a487c7839db14ab24b64a29c1f2bd3a7b2
Kevin Quick <kquick@galois.com>
2018-12-21 10:38:50 -0800

    Another use of isGap where functionality is based on gap status.

--- src/Draw/Messages.hs
+++ src/Draw/Messages.hs
@@ -77,12 +77,13 @@ renderChatMessage st hs ind renderTimeFunc msg =
                in Just $ withDefAttr emojiAttr $ txt ("   " <> reacMsg)
         msgTxt =
           case msg^.mUser of
-            NoUser ->
+            NoUser
+              | isGap msg -> withDefAttr gapMessageAttr m
+              | otherwise ->
                 case msg^.mType of
                     C DateTransition -> withDefAttr dateTransitionAttr (hBorderWithLabel m)
                     C NewMessagesTransition -> withDefAttr newMessageTransitionAttr (hBorderWithLabel m)
                     C Error -> withDefAttr errorMessageAttr m
-                    C UnknownGap -> withDefAttr gapMessageAttr m
                     _ -> withDefAttr clientMessageAttr m
             _ | isJoinLeave msg -> withDefAttr clientMessageAttr m
               | otherwise -> m
