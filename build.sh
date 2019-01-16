90febc3ad1b45d2b748f27ffd44f2d37375f2296
Martin Delille <martin@phonations.com>
2019-01-16 11:54:11 +0100

    fix osx compilation

--- build.sh
+++ build.sh
@@ -8,6 +8,16 @@
 
 set -e
 
+command_exists () {
+  type "$1" &> /dev/null ;
+}
+
+if [[ "$OSTYPE" = darwin* ]]; then
+  command_exists cabal || brew install cabal-install
+  cabal update
+  cabal install tasty-quickcheck
+fi
+
 HERE=$(cd `dirname $0`; pwd)
 cd $HERE
 cabal new-build -j --enable-tests
