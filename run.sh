9c5e1fa8ecacd58f172b149ff327420e5d86c05d
Kevin Quick <kquick@galois.com>
2019-03-15 11:06:55 -0700

    Further refinement of run.sh flexibility in location of executable.

--- run.sh
+++ run.sh
@@ -10,8 +10,10 @@ set -e
 HERE=$(cd `dirname $0`; pwd)
 
 # Portability note: -executable is only compatible with GNU find but
-# this invocation should be more portable.
-OPTIONS=$(find $HERE/dist-newstyle $HERE/dist -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
+# this invocation should be more portable.  Also be flexible about old
+# and new cabal output locations, preferring new, but avoiding
+# slurping up the entire current directory.
+OPTIONS=$(find $(for D in dist-newstyle dist; do [ -d $D ] && echo $D; done) -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
 if [ -z ${OPTIONS} ] ; then
     echo "No matterhorn executable found; try $ cabal new-build"
     exit 2
