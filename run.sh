ffa09ed5e2beee1c4e38bf0a010d26c895490051
Jonathan Daugherty <cygnus@foobox.com>
2019-03-18 08:56:49 -0700

    run.sh: improve naming conventions and help output

--- run.sh
+++ run.sh
@@ -9,26 +9,41 @@ set -e
 
 HERE=$(cd `dirname $0`; pwd)
 
+# An array of the build directories we'll look for.
+TRY_DIRS=("$HERE/dist-newstyle" "$HERE/dist")
+
+function none_found {
+    echo "No matterhorn build directories found. Looked for:"
+    for d in ${TRY_DIRS[@]}
+    do
+        echo "  $d"
+    done
+    echo "Try:"
+    echo "  $ cd ${HERE}"
+    echo "  $ cabal new-build"
+    exit 2
+}
+
 # Portability note: -executable is only compatible with GNU find but
 # this invocation should be more portable.  Also be flexible about old
 # and new cabal output locations, preferring new, but avoiding
 # slurping up the entire current directory.
-DIRS=$(for D in $HERE/dist-newstyle $HERE/dist; do [ -d $D ] && echo $D; done; true)
-if [ -z "${DIRS}" ] ; then
-    echo "No matterhorn build dirs found; try $ cabal new-build"
-    exit 2
+FOUND_DIRS=$(for D in ${TRY_DIRS[@]}; do [ -d $D ] && echo $D; done; true)
+if [ -z "${FOUND_DIRS}" ] ; then
+    none_found
 fi
-OPTIONS=$(find $DIRS -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
-if [ -z "${OPTIONS}" ] ; then
-    echo "No matterhorn executable found; try $ cabal new-build"
-    exit 2
+
+BINARIES=$(find $FOUND_DIRS -name matterhorn -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) )
+if [ -z "${BINARIES}" ] ; then
+    none_found
 fi
-SORTED=$(ls -t ${OPTIONS})
+
+SORTED_BINARIES=$(ls -t ${BINARIES})
 
 # Run the most recently-modified binary that we found. Note that since
 # we use exec, this loop never makes it past one iteration.
-for OPTION in ${SORTED}; do
-  exec "${OPTION}" ${1+$@}
+for BINARY in ${SORTED_BINARIES}; do
+  exec "${BINARY}" ${1+$@}
 done
 
 echo "No executables found."
