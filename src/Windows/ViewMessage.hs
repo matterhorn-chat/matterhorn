c209b296ce36e17b119e9f6c638b30461311ce43
Jonathan Daugherty <cygnus@foobox.com>
2019-09-10 15:03:43 -0700

    Windows.ViewMessage: flesh out tab entries

--- src/Windows/ViewMessage.hs
+++ src/Windows/ViewMessage.hs
@@ -12,6 +12,34 @@ import           Types
 
 viewMessageWindowTemplate :: TabbedWindowTemplate ViewMessageWindowTab
 viewMessageWindowTemplate =
-    TabbedWindowTemplate { twtEntries = []
+    TabbedWindowTemplate { twtEntries = [ messageEntry
+                                        , reactionsEntry
+                                        ]
                          , twtTitle = const $ txt "View Message"
                          }
+
+messageEntry :: TabbedWindowEntry ViewMessageWindowTab
+messageEntry =
+    TabbedWindowEntry { tweValue = VMTabMessage
+                      , tweRender = renderMessageTab
+                      , tweHandleEvent = handleEventMessageTab
+                      }
+
+reactionsEntry :: TabbedWindowEntry ViewMessageWindowTab
+reactionsEntry =
+    TabbedWindowEntry { tweValue = VMTabReactions
+                      , tweRender = renderReactionsTab
+                      , tweHandleEvent = handleEventReactionsTab
+                      }
+
+renderMessageTab :: ViewMessageWindowTab -> ChatState -> Widget Name
+renderMessageTab _ _ = txt ""
+
+renderReactionsTab :: ViewMessageWindowTab -> ChatState -> Widget Name
+renderReactionsTab _ _ = txt ""
+
+handleEventMessageTab :: ViewMessageWindowTab -> ChatState -> BrickEvent Name MHEvent -> EventM Name (Next ChatState)
+handleEventMessageTab _ s _ = continue s
+
+handleEventReactionsTab :: ViewMessageWindowTab -> ChatState -> BrickEvent Name MHEvent -> EventM Name (Next ChatState)
+handleEventReactionsTab _ s _ = continue s
