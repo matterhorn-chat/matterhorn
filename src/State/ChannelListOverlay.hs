matterhorn-chat/matterhorn

commit e7fefb9f20cdcd4c03a2bf8e8a7c83a9ccd745a3

Upgrade the channel list to use the ListOverlayState machinery

src/State/ChannelListOverlay.hs
@@ -1,0 +1,78 @@
+module State.ChannelListOverlay
+  ( enterChannelListOverlayMode
+
+  , channelListSelectDown
+  , channelListSelectUp
+  , channelListPageDown
+  , channelListPageUp
+  )
+where
+
+import           Prelude ()
+import           Prelude.MH
+
+import qualified Brick.Widgets.List as L
+import qualified Data.Vector as Vec
+import qualified Data.Sequence as Seq
+import           Data.Function ( on )
+
+import           Network.Mattermost.Types
+import qualified Network.Mattermost.Endpoints as MM
+
+import           State.ListOverlay
+import           State.Channels
+import           Types
+
+
+enterChannelListOverlayMode :: MH ()
+enterChannelListOverlayMode = do
+    myTId <- gets myTeamId
+    enterListOverlayMode csChannelListOverlay ChannelListOverlay
+        AllChannels enterHandler (fetchResults myTId)
+
+enterHandler :: Channel -> MH Bool
+enterHandler chan = do
+    joinChannel (getId chan)
+    return True
+
+fetchResults :: TeamId
+             -> ChannelSearchScope
+             -- ^ The scope to search
+             -> Session
+             -- ^ The connection session
+             -> Text
+             -- ^ The search string
+             -> IO (Vec.Vector Channel)
+fetchResults myTId AllChannels session searchString = do
+    resultChans <- MM.mmSearchChannels myTId searchString session
+    -- chans <- Seq.filter (\ c -> not (channelId c `elem` myChannels)) <$> loop mempty 0
+    let sortedChans = Vec.fromList $ toList $ Seq.sortBy (compare `on` channelName) resultChans
+    return sortedChans
+
+-- | Move the selection up in the channel list overlay by one channel.
+channelListSelectUp :: MH ()
+channelListSelectUp = channelListMove L.listMoveUp
+
+-- | Move the selection down in the channel list overlay by one channel.
+channelListSelectDown :: MH ()
+channelListSelectDown = channelListMove L.listMoveDown
+
+-- | Move the selection up in the channel list overlay by a page of channels
+-- (channelListPageSize).
+channelListPageUp :: MH ()
+channelListPageUp = channelListMove (L.listMoveBy (-1 * channelListPageSize))
+
+-- | Move the selection down in the channel list overlay by a page of channels
+-- (channelListPageSize).
+channelListPageDown :: MH ()
+channelListPageDown = channelListMove (L.listMoveBy channelListPageSize)
+
+-- | Transform the channel list results in some way, e.g. by moving the
+-- cursor, and then check to see whether the modification warrants a
+-- prefetch of more search results.
+channelListMove :: (L.List Name Channel -> L.List Name Channel) -> MH ()
+channelListMove = listOverlayMove csChannelListOverlay
+
+-- | The number of channels in a "page" for cursor movement purposes.
+channelListPageSize :: Int
+channelListPageSize = 10

src/Draw/ChannelListOverlay.hs
@@ -1,0 +1,49 @@
+module Draw.ChannelListOverlay
+  ( drawChannelListOverlay
+  )
+where
+
+import           Prelude ()
+import           Prelude.MH
+
+import           Brick
+import qualified Data.Text as T
+import           Text.Wrap ( defaultWrapSettings, preserveIndentation )
+
+import           Network.Mattermost.Types
+import           Network.Mattermost.Lenses
+
+import           Draw.Main
+import           Draw.ListOverlay ( drawListOverlay )
+import           Types
+import           Types.Common ( sanitizeUserText )
+
+
+drawChannelListOverlay :: ChatState -> [Widget Name]
+drawChannelListOverlay st =
+    let overlay = drawListOverlay (st^.csChannelListOverlay) channelSearchScopeHeader
+                                  channelSearchScopeNoResults channelSearchScopePrompt
+                                  renderChannel
+    in joinBorders overlay : drawMain False st
+
+channelSearchScopePrompt :: ChannelSearchScope -> Widget Name
+channelSearchScopePrompt scope =
+    txt $ case scope of
+        AllChannels -> "Search channels:"
+
+channelSearchScopeNoResults :: ChannelSearchScope -> Widget Name
+channelSearchScopeNoResults scope =
+    txt $ case scope of
+        AllChannels -> "No matching channels found."
+
+channelSearchScopeHeader :: ChannelSearchScope -> Widget Name
+channelSearchScopeHeader scope =
+    txt $ case scope of
+        AllChannels -> "Join a Channel"
+
+renderChannel :: Bool -> Channel -> Widget Name
+renderChannel _ chan =
+    let baseStr = (sanitizeUserText $ chan^.channelNameL) <> " (" <> (sanitizeUserText $ chan^.channelDisplayNameL) <> ")"
+        s = "  " <> (T.strip $ sanitizeUserText $ chan^.channelPurposeL)
+    in (vLimit 1 $ padRight Max $ txt baseStr) <=>
+       (vLimit 1 $ txtWrapWith (defaultWrapSettings { preserveIndentation = True }) s)

src/Events/ChannelListOverlay.hs
@@ -1,0 +1,28 @@
+module Events.ChannelListOverlay
+  ( onEventChannelListOverlay
+  , channelListOverlayKeybindings
+  )
+where
+
+import qualified Graphics.Vty as Vty
+
+import           Events.Keybindings
+import           State.ChannelListOverlay
+import           State.ListOverlay
+import           Types
+
+
+onEventChannelListOverlay :: Vty.Event -> MH ()
+onEventChannelListOverlay =
+    onEventListOverlay csChannelListOverlay channelListOverlayKeybindings
+
+-- | The keybindings we want to use while viewing a channel list overlay
+channelListOverlayKeybindings :: KeyConfig -> [Keybinding]
+channelListOverlayKeybindings = mkKeybindings
+    [ mkKb CancelEvent "Close the channel search list" (exitListOverlay csChannelListOverlay)
+    , mkKb SearchSelectUpEvent "Select the previous channel" channelListSelectUp
+    , mkKb SearchSelectDownEvent "Select the next channel" channelListSelectDown
+    , mkKb PageDownEvent "Page down in the channel list" channelListPageDown
+    , mkKb PageUpEvent "Page up in the channel list" channelListPageUp
+    , mkKb ActivateListItemEvent "Join the selected channel" (listOverlayActivateCurrent csChannelListOverlay)
+    ]

matterhorn.cabal
@@ -60,6 +60,7 @@ executable matterhorn
                        State.UrlSelect
                        State.ListOverlay
                        State.UserListOverlay
+                       State.ChannelListOverlay
                        State.Users
                        Zipper
                        Themes
@@ -69,13 +70,13 @@ executable matterhorn
                        Draw.Main
                        Draw.Messages
                        Draw.ChannelList
+                       Draw.ChannelListOverlay
                        Draw.ShowHelp
                        Draw.LeaveChannelConfirm
                        Draw.DeleteChannelConfirm
                        Draw.PostListOverlay
                        Draw.UserListOverlay
                        Draw.ListOverlay
-                       Draw.JoinChannel
                        Draw.URLList
                        Draw.Util
                        Draw.ViewMessage
@@ -87,11 +88,11 @@ executable matterhorn
                        Events.ShowHelp
                        Events.MessageSelect
                        Events.Main
-                       Events.JoinChannel
                        Events.ChannelSelect
                        Events.PostListOverlay
                        Events.UrlSelect
                        Events.UserListOverlay
+                       Events.ChannelListOverlay
                        Events.LeaveChannelConfirm
                        Events.DeleteChannelConfirm
                        Events.ViewMessage

src/Command.hs
@@ -32,6 +32,7 @@ import           State.Logging
 import           State.PostListOverlay
 import           State.Themes
 import           State.UserListOverlay
+import           State.ChannelListOverlay
 import           State.Users
 import           Types
 
@@ -102,8 +103,8 @@ commandList =
   , Cmd "leave" "Leave the current channel" NoArg $ \ () ->
       startLeaveCurrentChannel
 
-  , Cmd "join" "Browse the list of available channels" NoArg $ \ () ->
-      startJoinChannel
+  , Cmd "join" "Find a channel to join" NoArg $ \ () ->
+      enterChannelListOverlayMode
 
   , Cmd "join" "Join the specified channel" (TokenArg "channel" NoArg) $ \(n, ()) ->
       joinChannelByName n

src/Draw.hs
@@ -8,12 +8,12 @@ import Prelude.MH
 import Brick
 
 import Draw.DeleteChannelConfirm
-import Draw.JoinChannel
 import Draw.LeaveChannelConfirm
 import Draw.Main
 import Draw.PostListOverlay
 import Draw.ShowHelp
 import Draw.UserListOverlay
+import Draw.ChannelListOverlay
 import Draw.ViewMessage
 import Draw.ManageAttachments
 import Types
@@ -27,12 +27,12 @@ draw st =
         ShowHelp topic             -> drawShowHelp topic st
         ChannelSelect              -> drawMain True st
         LeaveChannelConfirm        -> drawLeaveChannelConfirm st
-        JoinChannel                -> drawJoinChannel st
         MessageSelect              -> drawMain True st
         MessageSelectDeleteConfirm -> drawMain True st
         DeleteChannelConfirm       -> drawDeleteChannelConfirm st
         PostListOverlay contents   -> drawPostListOverlay contents st
         UserListOverlay            -> drawUserListOverlay st
+        ChannelListOverlay         -> drawChannelListOverlay st
         ViewMessage                -> drawViewMessage st
         ManageAttachments          -> drawManageAttachments st
         ManageAttachmentsBrowseFiles -> drawManageAttachments st

src/Draw/JoinChannel.hs
@@ -1,56 +1,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Draw.JoinChannel
-    ( drawJoinChannel
-    )
-where
-
-import           Prelude ()
-import           Prelude.MH
-
-import           Brick
-import           Brick.Widgets.Border
-import           Brick.Widgets.Center
-import           Brick.Widgets.List
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import           Text.Wrap ( defaultWrapSettings, preserveIndentation )
-
-import           Network.Mattermost.Lenses ( channelDisplayNameL , channelNameL
-                                           , channelPurposeL )
-import           Network.Mattermost.Types ( Channel )
-
-import           Draw.Main
-import           Themes
-import           Types
-import           Types.Common
-
-
-drawJoinChannel :: ChatState -> [Widget Name]
-drawJoinChannel st = (joinBorders $ joinChannelBox st) : (drawMain False st)
-
-joinChannelBox :: ChatState -> Widget Name
-joinChannelBox st =
-    let chList = case st^.csJoinChannelList of
-          Nothing -> center $ withDefAttr dialogEmphAttr $ txt "[Loading channel list]"
-          Just chanList -> renderList renderJoinListItem True chanList
-        highlight = withDefAttr dialogEmphAttr
-        cur = maybe 0 ((+1) . fst) (listSelectedElement =<< st^.csJoinChannelList)
-        len = maybe 0 (\l -> V.length $ l^.listElementsL) $ st^.csJoinChannelList
-    in centerLayer $
-       vLimit 20 $
-       hLimit 60 $
-       withDefAttr dialogAttr $
-       borderWithLabel (txt "Join Channel") $
-       vBox [ chList
-            , hBorderWithLabel (str $ (show cur) <> "/" <> (show len))
-            , hCenter $ txt "Use " <+> (highlight $ txt "arrow keys") <+> txt " and " <+>
-                        (highlight $ txt "Enter") <+> txt " to select a channel"
-            , hCenter $ txt "to join or press " <+> (highlight $ txt "Esc") <+> txt " to cancel."
-            ]
-
-renderJoinListItem :: Bool -> Channel -> Widget Name
-renderJoinListItem _ chan =
-    let baseStr = (sanitizeUserText $ chan^.channelNameL) <> " (" <> (sanitizeUserText $ chan^.channelDisplayNameL) <> ")"
-        s = "  " <> (T.strip $ sanitizeUserText $ chan^.channelPurposeL)
-    in (vLimit 1 $ padRight Max $ txt baseStr) <=>
-       (vLimit 1 $ txtWrapWith (defaultWrapSettings { preserveIndentation = True }) s)

src/Draw/ShowHelp.hs
@@ -28,6 +28,7 @@ import           Events.PostListOverlay
 import           Events.ShowHelp
 import           Events.UrlSelect
 import           Events.UserListOverlay
+import           Events.ChannelListOverlay
 import           Events.ManageAttachments
 import           Events.ViewMessage
 import           HelpTopics ( helpTopics )
@@ -375,6 +376,7 @@ keybindSections kc =
     , ("Text Editing", editingKeybindings)
     , ("Flagged Messages", postListOverlayKeybindings kc)
     , ("User Listings", userListOverlayKeybindings kc)
+    , ("Channel Search Window", channelListOverlayKeybindings kc)
     , ("Message Viewer", viewMessageKeybindings kc)
     , ("Attachment List", attachmentListKeybindings kc)
     ]

commit 26b824c99314a6cb41260cf3480689a01879940e

Fix bug where /join channel list included channels the user is a member of (fixes #602)
This fixes a regression that was introduced in e7fefb9f.

src/State/ChannelListOverlay.hs
@@ -15,6 +15,7 @@ import qualified Brick.Widgets.List as L
 import qualified Data.Vector as Vec
 import qualified Data.Sequence as Seq
 import           Data.Function ( on )
+import           Lens.Micro.Platform ( to )
 
 import           Network.Mattermost.Types
 import qualified Network.Mattermost.Endpoints as MM
@@ -27,8 +28,9 @@ import           Types
 enterChannelListOverlayMode :: MH ()
 enterChannelListOverlayMode = do
     myTId <- gets myTeamId
+    myChannels <- use (csChannels.to (filteredChannelIds (const True)))
     enterListOverlayMode csChannelListOverlay ChannelListOverlay
-        AllChannels enterHandler (fetchResults myTId)
+        AllChannels enterHandler (fetchResults myTId myChannels)
 
 enterHandler :: Channel -> MH Bool
 enterHandler chan = do
@@ -36,6 +38,8 @@ enterHandler chan = do
     return True
 
 fetchResults :: TeamId
+             -> [ChannelId]
+             -- ^ The channels to exclude from the results
              -> ChannelSearchScope
              -- ^ The scope to search
              -> Session
@@ -43,10 +47,10 @@ fetchResults :: TeamId
              -> Text
              -- ^ The search string
              -> IO (Vec.Vector Channel)
-fetchResults myTId AllChannels session searchString = do
+fetchResults myTId exclude AllChannels session searchString = do
     resultChans <- MM.mmSearchChannels myTId searchString session
-    -- chans <- Seq.filter (\ c -> not (channelId c `elem` myChannels)) <$> loop mempty 0
-    let sortedChans = Vec.fromList $ toList $ Seq.sortBy (compare `on` channelName) resultChans
+    let filteredChans = Seq.filter (\ c -> not (channelId c `elem` exclude)) resultChans
+        sortedChans = Vec.fromList $ toList $ Seq.sortBy (compare `on` channelName) filteredChans
     return sortedChans
 
 -- | Move the selection up in the channel list overlay by one channel.