a681f3d9df5e5373f5280365701f526abab1c225
Jonathan Daugherty <cygnus@foobox.com>
2020-09-14 11:56:26 -0700

    Add scripts/mkrelease.sh, a script to coordinate release builds on our build hosts

---
+++ scripts/mkrelease.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# This script starts a Tmux session to make a release on all release
+# hosts. The script makes some assumptions:
+#
+# * You are on the Galois, Inc. VPN., which is required to access the
+#   build hosts. This script is not intended to be run by third
+#   parties; if you want to make a release for your platform, use
+#   scripts/local-mkrelease.sh.
+# * You have tmux installed locally.
+# * Each build host has cabal and GHC in its PATH.
+# * You can authenticate to each build host with your SSH key.
+# * The Github host key is authorized on all target hosts.
+#
+# The script does NOT assume that Matterhorn is cloned on the target
+# host, and it will take care of that when necessary.
+
+set -e
+
+HERE=$(cd `dirname $0`; pwd)
+ROOT=$HERE/..
+REPO=git@github.com:matterhorn-chat/matterhorn.git
+SESSION=matterhorn-release
+RELEASE_BRANCH=develop
+
+CMD="([ -d matterhorn ] || git clone $REPO) && cd matterhorn && git checkout $RELEASE_BRANCH && git pull && bash scripts/vm-mkrelease-wrapper.sh"
+
+tmux \
+    new-session -d -s $SESSION -n "ubuntu-1804" "ssh -A galois@vm-35-dd.eic.galois.com '$CMD'" \; \
+    set-option -t $SESSION:0 remain-on-exit on \; \
+    new-window -d -t $SESSION -n "ubuntu-2004" "ssh -A galois@vm-36-83.eic.galois.com '$CMD'" \; \
+    set-option -t $SESSION:1 remain-on-exit on \; \
+    new-window -d -t $SESSION -n "fedora" "ssh -A galois@vm-35-de.eic.galois.com '$CMD'" \; \
+    set-option -t $SESSION:2 remain-on-exit on \; \
+    new-window -d -t $SESSION -n "centos" "ssh -A galois@vm-35-a7.eic.galois.com '$CMD'" \; \
+    set-option -t $SESSION:3 remain-on-exit on
