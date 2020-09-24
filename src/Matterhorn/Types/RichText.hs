5935daccf43b64ce8d851cdf921a86e8dcff2603
Jonathan Daugherty <cygnus@foobox.com>
2020-09-24 13:04:03 -0700

    Types.RichText: only parse channel references with non-empty channel names

--- src/Matterhorn/Types/RichText.hs
+++ src/Matterhorn/Types/RichText.hs
@@ -258,11 +258,12 @@ fromMarkdownInlines inlines =
                   t' = T.concat $ t : (unsafeGetStr <$> F.toList uFrags)
                   u = T.drop 1 t'
               in Element sty (EUser u) <| go sty rest
-          C.Str t :< xs | normalChannelSigil `T.isPrefixOf` t &&
-                          (T.length t > T.length normalChannelSigil) ->
+          C.Str t :< xs | normalChannelSigil `T.isPrefixOf` t ->
               let (cFrags, rest) = Seq.spanl isNameFragment xs
                   cn = T.drop 1 $ T.concat $ t : (unsafeGetStr <$> F.toList cFrags)
-              in Element sty (EChannel cn) <| go sty rest
+              in if not (T.null cn)
+                 then Element sty (EChannel cn) <| go sty rest
+                 else Element sty (EText normalChannelSigil) <| go sty xs
           C.Str t :< xs ->
               -- When we encounter a string node, we go ahead and
               -- process the rest of the nodes in the sequence. If the
