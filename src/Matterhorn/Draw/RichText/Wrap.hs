9db1e954648a97d26c7d1e8bd9a152925e06fec9
Jonathan Daugherty <cygnus@foobox.com>
2020-10-28 11:40:42 -0700

    Draw.RichText.Wrap: document

--- src/Matterhorn/Draw/RichText/Wrap.hs
+++ src/Matterhorn/Draw/RichText/Wrap.hs
@@ -1,3 +1,17 @@
+-- | This module performs line-wrapping of sequences of flattend inline
+-- values produced by 'flattenInlineSeq'.
+--
+-- This process works by maintaining a 'WrapState' in the 'WrapM'
+-- monad, where inline values are pushed onto the current line, and
+-- line breaks are introduced as inlines exceed the available width.
+-- The most important caveat of this module is that wrapping depends
+-- on knowing the width of each 'FlattenedValue', which is provided
+-- by the 'fvWidth' function. But 'fvWidth' must return values that
+-- are consistent with the how the inlines actually get rendered by
+-- 'renderFlattenedValue'. This is because there are visual aspects to
+-- how some inlines get rendered that are implicit, such as user or
+-- channel sigils that get added at drawing time, that have an impact on
+-- their visible width.
 module Matterhorn.Draw.RichText.Wrap
   ( WrappedLine
   , doLineWrapping
@@ -24,13 +38,20 @@ type WrappedLine = Seq FlattenedValue
 
 data WrapState =
     WrapState { wrapCompletedLines :: Seq WrappedLine
+              -- ^ The completed lines so far
               , wrapCurLine :: WrappedLine
+              -- ^ The current line we are accumulating
               , wrapCurCol :: Int
+              -- ^ The width of wrapCurLine, in columns
               , wrapWidth :: Int
+              -- ^ The maximum allowable width
               }
 
 type WrapM a = State WrapState a
 
+-- | Push a flattened value onto the current line if possible, or add a
+-- line break and add the inline value to a new line if it would cause
+-- the current line width to exceed the maximum.
 pushValue :: FlattenedValue -> WrapM ()
 pushValue i = do
     let iw = fvWidth i
@@ -46,6 +67,8 @@ pushValue i = do
 
     pushThisInline
 
+-- | Insert a new line break by moving the current accumulating line
+-- onto the completed lines list and resetting it to empty.
 pushLine :: WrapM ()
 pushLine = do
     let trimLeadingWhitespace s =
@@ -59,6 +82,10 @@ pushLine = do
            , wrapCurCol = 0
            }
 
+-- | Given a maximum width and an inline sequence, produce a sequence of
+-- lines wrapped at the specified column. This only returns lines longer
+-- than the maximum width when those lines have a single inline value
+-- that cannot be broken down further (such as a long URL).
 doLineWrapping :: Int -> Seq FlattenedValue -> Seq WrappedLine
 doLineWrapping maxCols i =
     result
