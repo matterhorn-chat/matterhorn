433a8faaa7374002df9bbe6ca5b80e9780d418e1
Jonathan Daugherty <cygnus@foobox.com>
2020-11-23 11:44:44 -0800

    Whitespace

--- src/Matterhorn/Events/ThemeListOverlay.hs
+++ src/Matterhorn/Events/ThemeListOverlay.hs
@@ -13,7 +13,8 @@ import           Matterhorn.Types
 
 onEventThemeListOverlay :: Vty.Event -> MH ()
 onEventThemeListOverlay =
-    void . onEventListOverlay (csCurrentTeam.tsThemeListOverlay) themeListOverlayKeybindings
+    void . onEventListOverlay (csCurrentTeam.tsThemeListOverlay)
+        themeListOverlayKeybindings
 
 -- | The keybindings we want to use while viewing a user list overlay
 themeListOverlayKeybindings :: KeyConfig -> KeyHandlerMap
@@ -21,10 +22,16 @@ themeListOverlayKeybindings = mkKeybindings themeListOverlayKeyHandlers
 
 themeListOverlayKeyHandlers :: [KeyEventHandler]
 themeListOverlayKeyHandlers =
-    [ mkKb CancelEvent "Close the theme list" (exitListOverlay (csCurrentTeam.tsThemeListOverlay))
-    , mkKb SearchSelectUpEvent "Select the previous theme" themeListSelectUp
-    , mkKb SearchSelectDownEvent "Select the next theme" themeListSelectDown
-    , mkKb PageDownEvent "Page down in the theme list" themeListPageDown
-    , mkKb PageUpEvent "Page up in the theme list" themeListPageUp
-    , mkKb ActivateListItemEvent "Switch to the selected color theme" (listOverlayActivateCurrent (csCurrentTeam.tsThemeListOverlay))
+    [ mkKb CancelEvent "Close the theme list"
+      (exitListOverlay (csCurrentTeam.tsThemeListOverlay))
+    , mkKb SearchSelectUpEvent "Select the previous theme"
+      themeListSelectUp
+    , mkKb SearchSelectDownEvent "Select the next theme"
+      themeListSelectDown
+    , mkKb PageDownEvent "Page down in the theme list"
+      themeListPageDown
+    , mkKb PageUpEvent "Page up in the theme list"
+      themeListPageUp
+    , mkKb ActivateListItemEvent "Switch to the selected color theme"
+      (listOverlayActivateCurrent (csCurrentTeam.tsThemeListOverlay))
     ]
