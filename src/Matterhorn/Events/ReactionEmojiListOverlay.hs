568c91daf01db35d510abc1521f027e926952c3c
Jonathan Daugherty <cygnus@foobox.com>
2020-11-23 11:40:09 -0800

    Whitespace

--- src/Matterhorn/Events/ReactionEmojiListOverlay.hs
+++ src/Matterhorn/Events/ReactionEmojiListOverlay.hs
@@ -18,7 +18,8 @@ import           Matterhorn.Types
 
 onEventReactionEmojiListOverlay :: Vty.Event -> MH ()
 onEventReactionEmojiListOverlay =
-    void . onEventListOverlay (csCurrentTeam.tsReactionEmojiListOverlay) reactionEmojiListOverlayKeybindings
+    void . onEventListOverlay (csCurrentTeam.tsReactionEmojiListOverlay)
+           reactionEmojiListOverlayKeybindings
 
 -- | The keybindings we want to use while viewing an emoji list overlay
 reactionEmojiListOverlayKeybindings :: KeyConfig -> KeyHandlerMap
@@ -26,10 +27,16 @@ reactionEmojiListOverlayKeybindings = mkKeybindings reactionEmojiListOverlayKeyH
 
 reactionEmojiListOverlayKeyHandlers :: [KeyEventHandler]
 reactionEmojiListOverlayKeyHandlers =
-    [ mkKb CancelEvent "Close the emoji search window" (exitListOverlay (csCurrentTeam.tsReactionEmojiListOverlay))
-    , mkKb SearchSelectUpEvent "Select the previous emoji" reactionEmojiListSelectUp
-    , mkKb SearchSelectDownEvent "Select the next emoji" reactionEmojiListSelectDown
-    , mkKb PageDownEvent "Page down in the emoji list" reactionEmojiListPageDown
-    , mkKb PageUpEvent "Page up in the emoji list" reactionEmojiListPageUp
-    , mkKb ActivateListItemEvent "Post the selected emoji reaction" (listOverlayActivateCurrent (csCurrentTeam.tsReactionEmojiListOverlay))
+    [ mkKb CancelEvent "Close the emoji search window"
+      (exitListOverlay (csCurrentTeam.tsReactionEmojiListOverlay))
+    , mkKb SearchSelectUpEvent "Select the previous emoji"
+      reactionEmojiListSelectUp
+    , mkKb SearchSelectDownEvent "Select the next emoji"
+      reactionEmojiListSelectDown
+    , mkKb PageDownEvent "Page down in the emoji list"
+      reactionEmojiListPageDown
+    , mkKb PageUpEvent "Page up in the emoji list"
+      reactionEmojiListPageUp
+    , mkKb ActivateListItemEvent "Post the selected emoji reaction"
+      (listOverlayActivateCurrent (csCurrentTeam.tsReactionEmojiListOverlay))
     ]
