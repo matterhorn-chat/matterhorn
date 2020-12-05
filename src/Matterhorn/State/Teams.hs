cf00ae6f90bfde333f56a80b9184cb03898974b2
Jonathan Daugherty <cygnus@foobox.com>
2020-12-05 07:55:16 -0800

    nextTeam/prevTeam: update destination team view status and fetch messages

--- src/Matterhorn/State/Teams.hs
+++ src/Matterhorn/State/Teams.hs
@@ -10,13 +10,21 @@ import           Matterhorn.Prelude
 import           Lens.Micro.Platform ( (%=) )
 
 import           Matterhorn.Types
+import           Matterhorn.State.Channels
+import           Matterhorn.State.Messages
 import qualified Matterhorn.Zipper as Z
 
 
 nextTeam :: MH ()
 nextTeam = do
     csTeamZipper %= Z.right
+    postChangeTeamCommon
 
 prevTeam :: MH ()
 prevTeam = do
     csTeamZipper %= Z.left
+
+postChangeTeamCommon :: MH ()
+postChangeTeamCommon = do
+    updateViewed False
+    fetchVisibleIfNeeded
