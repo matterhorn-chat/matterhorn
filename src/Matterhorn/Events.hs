7b859503c02386663e80a96754049ae0a588a592
Jonathan Daugherty <cygnus@foobox.com>
2021-08-22 14:30:16 -0700

    Events: move all mouse event handlers to Matterhorn.Events.Mouse

--- src/Matterhorn/Events.hs
+++ src/Matterhorn/Events.hs
@@ -26,8 +26,6 @@ import           Matterhorn.State.Channels
 import           Matterhorn.State.Common
 import           Matterhorn.State.Help
 import           Matterhorn.State.Messages
-import           Matterhorn.State.Teams ( setTeam )
-import           Matterhorn.State.ListOverlay ( listOverlayActivate )
 import           Matterhorn.Types
 
 import           Matterhorn.Events.ChannelSelect
@@ -45,12 +43,11 @@ import           Matterhorn.Events.UrlSelect
 import           Matterhorn.Events.UserListOverlay
 import           Matterhorn.Events.ChannelListOverlay
 import           Matterhorn.Events.ReactionEmojiListOverlay
-import           Matterhorn.State.Reactions ( toggleReaction )
 import           Matterhorn.Events.TabbedWindow
 import           Matterhorn.Events.ManageAttachments
+import           Matterhorn.Events.Mouse
 import           Matterhorn.Events.EditNotifyPrefs
 import           Matterhorn.Events.Websocket
-import           Matterhorn.State.Links ( openLinkTarget )
 
 onEvent :: ChatState -> BrickEvent Name MHEvent -> EventM Name (Next ChatState)
 onEvent st ev = runMHEvent st $ do
@@ -81,15 +78,6 @@ onBrickEvent e@(MouseDown n button modifier clickLoc) = do
         csLastMouseDownEvent .= Just e
         mode <- use (csCurrentTeam.tsMode)
         mouseHandlerByMode mode n button modifier clickLoc
-
-mouseHandlerByMode :: Mode -> Name -> Vty.Button -> [Vty.Modifier] -> Location -> MH ()
-mouseHandlerByMode mode =
-    case mode of
-        ChannelSelect            -> channelSelectMouseHandler
-        EditNotifyPrefs          -> editNotifyPrefsMouseHandler
-        ReactionEmojiListOverlay -> reactionEmojiListMouseHandler
-        UrlSelect                -> urlListMouseHandler
-        _                        -> globalMouseHandler
 
 onAppEvent :: MHEvent -> MH ()
 onAppEvent RefreshWebsocketEvent =
@@ -156,79 +144,6 @@ handleIEvent (LogStartFailed path err) =
 handleIEvent (LogSnapshotFailed path err) =
     postErrorMessage' $ "Could not write log snapshot to " <> T.pack path <>
                         ", error: " <> T.pack err
-
--- Handle mouse click events.
---
--- Note that the handler for each case may need to check the application
--- mode before handling the click. This is because some mouse events
--- only make sense when the UI is displaying certain contents. While
--- it's true that we probably wouldn't even get the click events in the
--- first place (because the UI element would only cause a click event
--- to be reported if it was actually rendered), there are cases when we
--- can get clicks on UI elements that *are* clickable even though those
--- clicks don't make sense for the application mode. A concrete example
--- of this is when we display the current channel's contents in one
--- layer, in monochrome, and then display a modal dialog box on top of
--- that. We probably *should* ignore clicks on the lower layer because
--- that's not the mode the application is in, but getting that right
--- could be hard because we'd have to figure out all possible modes
--- where those lower-layer clicks would be nonsensical. We don't bother
--- doing that in the harder cases; instead we just handle the clicks
--- and do what we would ordinarily do, assuming that there's no real
--- harm done. The worst that could happen is that a user could click
--- accidentally on a grayed-out URL (in a message, say) next to a modal
--- dialog box and then see the URL get opened. That would be weird, but
--- it isn't the end of the world.
-globalMouseHandler :: Name -> Vty.Button -> [Vty.Modifier] -> Location -> MH ()
-globalMouseHandler (ClickableChannelListEntry channelId) Vty.BLeft [] _ = do
-    whenMode Main $ do
-        resetReturnChannel
-        setFocus channelId
-        setMode Main
-globalMouseHandler (ClickableTeamListEntry teamId) Vty.BLeft [] _ =
-    -- We deliberately handle this event in all modes; this allows us to
-    -- switch the UI to another team regardless of what state it is in,
-    -- which is by design since all teams have their own UI states.
-    setTeam teamId
-globalMouseHandler (ClickableURLInMessage _ _ t) Vty.BLeft [] _ =
-    void $ openLinkTarget t
-globalMouseHandler (ClickableURL _ _ t) Vty.BLeft [] _ =
-    void $ openLinkTarget t
-globalMouseHandler (ClickableUsernameInMessage _ _ username) Vty.BLeft [] _ =
-    changeChannelByName $ userSigil <> username
-globalMouseHandler (ClickableUsername _ _ username) Vty.BLeft [] _ =
-    changeChannelByName $ userSigil <> username
-globalMouseHandler (ClickableAttachment fId) Vty.BLeft [] _ =
-    void $ openLinkTarget $ LinkFileId fId
-globalMouseHandler (ClickableReactionInMessage pId t uIds) Vty.BLeft [] _ =
-    void $ toggleReaction pId t uIds
-globalMouseHandler (ClickableReaction pId t uIds) Vty.BLeft [] _ =
-    void $ toggleReaction pId t uIds
-globalMouseHandler _ _ _ _ =
-    return ()
-
-editNotifyPrefsMouseHandler :: Name -> Vty.Button -> [Vty.Modifier] -> Location -> MH ()
-editNotifyPrefsMouseHandler n b mods l =
-    handleEditNotifyPrefsEvent (MouseDown n b mods l)
-
-urlListMouseHandler :: Name -> Vty.Button -> [Vty.Modifier] -> Location -> MH ()
-urlListMouseHandler (ClickableURLListEntry _ t) Vty.BLeft [] _ =
-    void $ openLinkTarget t
-urlListMouseHandler _ _ _ _ =
-    return ()
-
-channelSelectMouseHandler :: Name -> Vty.Button -> [Vty.Modifier] -> Location -> MH ()
-channelSelectMouseHandler (ChannelSelectEntry match) Vty.BLeft [] _ = do
-    setMode Main
-    setFocus $ channelListEntryChannelId $ matchEntry match
-channelSelectMouseHandler _ _ _ _ =
-    return ()
-
-reactionEmojiListMouseHandler :: Name -> Vty.Button -> [Vty.Modifier] -> Location -> MH ()
-reactionEmojiListMouseHandler (ReactionEmojiListOverlayEntry val) Vty.BLeft [] _ =
-    listOverlayActivate (csCurrentTeam.tsReactionEmojiListOverlay) val
-reactionEmojiListMouseHandler _ _ _ _ =
-    return ()
 
 formatError :: MHError -> T.Text
 formatError (GenericError msg) =
