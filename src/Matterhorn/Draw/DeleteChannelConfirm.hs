1072703517bd3bdfde8d5dc3f6ae37e9b974e8d5
Jonathan Daugherty <cygnus@foobox.com>
2021-10-05 14:15:31 -0700

    Draw.DeleteChannelConfirm: handle missing current channel

--- src/Matterhorn/Draw/DeleteChannelConfirm.hs
+++ src/Matterhorn/Draw/DeleteChannelConfirm.hs
@@ -19,12 +19,16 @@ import Matterhorn.Types
 
 drawDeleteChannelConfirm :: ChatState -> TeamId -> Widget Name
 drawDeleteChannelConfirm st tId =
-    let cName = st^.csCurrentChannel(tId).ccInfo.cdName
-    in centerLayer $ hLimit 50 $ vLimit 15 $
-       withDefAttr dialogAttr $
-       borderWithLabel (txt "Confirm Delete Channel") $
-       vBox [ padBottom (Pad 1) $ hCenter $ txt "Are you sure you want to delete this channel?"
-            , padBottom (Pad 1) $ hCenter $ withDefAttr dialogEmphAttr $ txt cName
-            , hCenter $ txt "Press " <+> (withDefAttr dialogEmphAttr $ txt "Y") <+> txt " to delete the channel"
-            , hCenter $ txt "or any other key to cancel."
-            ]
+    let cId = st^.csCurrentChannelId(tId)
+    in case st^?csChannel(cId) of
+        Nothing -> emptyWidget
+        Just chan ->
+            let cName = chan^.ccInfo.cdName
+            in centerLayer $ hLimit 50 $ vLimit 15 $
+               withDefAttr dialogAttr $
+               borderWithLabel (txt "Confirm Delete Channel") $
+               vBox [ padBottom (Pad 1) $ hCenter $ txt "Are you sure you want to delete this channel?"
+                    , padBottom (Pad 1) $ hCenter $ withDefAttr dialogEmphAttr $ txt cName
+                    , hCenter $ txt "Press " <+> (withDefAttr dialogEmphAttr $ txt "Y") <+> txt " to delete the channel"
+                    , hCenter $ txt "or any other key to cancel."
+                    ]
