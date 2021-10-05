03b866eaf44911683f608c2085cbcfcde2adc60a
Jonathan Daugherty <cygnus@foobox.com>
2021-10-05 14:14:31 -0700

    Draw.LeaveChannelConfirm: handle missing current channel

--- src/Matterhorn/Draw/LeaveChannelConfirm.hs
+++ src/Matterhorn/Draw/LeaveChannelConfirm.hs
@@ -19,12 +19,16 @@ import Matterhorn.Types
 
 drawLeaveChannelConfirm :: ChatState -> TeamId -> Widget Name
 drawLeaveChannelConfirm st tId =
-    let cName = st^.csCurrentChannel(tId).ccInfo.cdName
-    in centerLayer $ hLimit 50 $ vLimit 15 $
-       withDefAttr dialogAttr $
-       borderWithLabel (txt "Confirm Leave Channel") $
-       vBox [ padBottom (Pad 1) $ hCenter $ txt "Are you sure you want to leave this channel?"
-            , padBottom (Pad 1) $ hCenter $ withDefAttr dialogEmphAttr $ txt cName
-            , hCenter $ txt "Press " <+> (withDefAttr dialogEmphAttr $ txt "Y") <+> txt " to leave the channel"
-            , hCenter $ txt "or any other key to cancel."
-            ]
+    let cId = st^.csCurrentChannelId(tId)
+    in case st^?csChannel(cId) of
+        Nothing -> emptyWidget
+        Just chan ->
+            let cName = chan^.ccInfo.cdName
+            in centerLayer $ hLimit 50 $ vLimit 15 $
+               withDefAttr dialogAttr $
+               borderWithLabel (txt "Confirm Leave Channel") $
+               vBox [ padBottom (Pad 1) $ hCenter $ txt "Are you sure you want to leave this channel?"
+                    , padBottom (Pad 1) $ hCenter $ withDefAttr dialogEmphAttr $ txt cName
+                    , hCenter $ txt "Press " <+> (withDefAttr dialogEmphAttr $ txt "Y") <+> txt " to leave the channel"
+                    , hCenter $ txt "or any other key to cancel."
+                    ]
