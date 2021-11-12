f919acfc9046c9968f5bfc770ed700dd7dd71d3a
Jonathan Daugherty <cygnus@foobox.com>
2021-11-12 09:11:59 -0800

    shouldIgnore: factor out substring list

--- src/Matterhorn/State/Setup/Threads.hs
+++ src/Matterhorn/State/Setup/Threads.hs
@@ -367,13 +367,17 @@ rateLimitRetry rateLimitNotify act = do
 shouldIgnore :: SomeException -> Bool
 shouldIgnore e =
     let eStr = show e
-    in or [ "getAddrInfo" `isInfixOf` eStr
-          , "Network.Socket.recvBuf" `isInfixOf` eStr
-          , "Network.Socket.sendBuf" `isInfixOf` eStr
-          , "resource vanished" `isInfixOf` eStr
-          , "timeout" `isInfixOf` eStr
-          , "partial packet" `isInfixOf` eStr
-          , "No route to host" `isInfixOf` eStr
-          , "(5,0,3)" `isInfixOf` eStr
-          , "(5,0,4)" `isInfixOf` eStr
-          ]
+    in or $ (`isInfixOf` eStr) <$> ignoreErrorSubstrings
+
+ignoreErrorSubstrings :: [String]
+ignoreErrorSubstrings =
+    [ "getAddrInfo"
+    , "Network.Socket.recvBuf"
+    , "Network.Socket.sendBuf"
+    , "resource vanished"
+    , "timeout"
+    , "partial packet"
+    , "No route to host"
+    , "(5,0,3)"
+    , "(5,0,4)"
+    ]
