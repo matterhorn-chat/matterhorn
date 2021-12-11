cf01a93e6ee9f415c22cc6f6c0b3cd81cbab139d
Jonathan Daugherty <cygnus@foobox.com>
2021-12-11 15:48:47 -0800

    LastRunState: style nits

--- src/Matterhorn/LastRunState.hs
+++ src/Matterhorn/LastRunState.hs
@@ -35,39 +35,39 @@ import           Matterhorn.Types
 
 -- | Run state of the program. This is saved in a file on program exit and
 -- | looked up from the file on program startup.
-data LastRunState = LastRunState
-  { _lrsHost              :: Hostname  -- ^ Host of the server
-  , _lrsPort              :: Port      -- ^ Post of the server
-  , _lrsUserId            :: UserId    -- ^ ID of the logged-in user
-  , _lrsSelectedChannelId :: Maybe ChannelId -- ^ ID of the last selected channel
-  }
+data LastRunState =
+    LastRunState { _lrsHost              :: Hostname  -- ^ Host of the server
+                 , _lrsPort              :: Port      -- ^ Post of the server
+                 , _lrsUserId            :: UserId    -- ^ ID of the logged-in user
+                 , _lrsSelectedChannelId :: Maybe ChannelId -- ^ ID of the last selected channel
+                 }
 
 instance A.ToJSON LastRunState where
-  toJSON lrs = A.object [ "host"           A..= _lrsHost lrs
-                        , "port"           A..= _lrsPort lrs
-                        , "user_id"        A..= _lrsUserId lrs
-                        , "sel_channel_id" A..= _lrsSelectedChannelId lrs
-                        ]
+    toJSON lrs = A.object [ "host"           A..= _lrsHost lrs
+                          , "port"           A..= _lrsPort lrs
+                          , "user_id"        A..= _lrsUserId lrs
+                          , "sel_channel_id" A..= _lrsSelectedChannelId lrs
+                          ]
 
 instance A.FromJSON LastRunState where
-  parseJSON = A.withObject "LastRunState" $ \v ->
-    LastRunState
-    <$> v A..: "host"
-    <*> v A..: "port"
-    <*> v A..: "user_id"
-    <*> v A..: "sel_channel_id"
+    parseJSON = A.withObject "LastRunState" $ \v ->
+        LastRunState
+        <$> v A..: "host"
+        <*> v A..: "port"
+        <*> v A..: "user_id"
+        <*> v A..: "sel_channel_id"
 
 makeLenses ''LastRunState
 
 toLastRunState :: ChatState -> LastRunState
-toLastRunState cs = LastRunState
-  { _lrsHost              = cs^.csResources.crConn.cdHostnameL
-  , _lrsPort              = cs^.csResources.crConn.cdPortL
-  , _lrsUserId            = myUserId cs
-  , _lrsSelectedChannelId = do
-      tId <- cs^.csCurrentTeamId
-      cs^.csCurrentChannelId(tId)
-  }
+toLastRunState cs =
+    LastRunState { _lrsHost              = cs^.csResources.crConn.cdHostnameL
+                 , _lrsPort              = cs^.csResources.crConn.cdPortL
+                 , _lrsUserId            = myUserId cs
+                 , _lrsSelectedChannelId = do
+                     tId <- cs^.csCurrentTeamId
+                     cs^.csCurrentChannelId(tId)
+                 }
 
 lastRunStateFileMode :: P.FileMode
 lastRunStateFileMode = P.unionFileModes P.ownerReadMode P.ownerWriteMode
@@ -97,15 +97,16 @@ writeLastRunState cs tId =
 -- | Reads the last run state from a file given the current team ID.
 readLastRunState :: TeamId -> IO (Either String LastRunState)
 readLastRunState tId = runExceptT $ do
-  contents <- convertIOException $
-    lastRunStateFilePath (unId $ toId tId) >>= BS.readFile
-  case A.eitherDecodeStrict' contents of
-    Right val -> return val
-    Left err -> throwE $ "Failed to parse lastRunState file: " ++ err
+    contents <- convertIOException $
+        lastRunStateFilePath (unId $ toId tId) >>= BS.readFile
+    case A.eitherDecodeStrict' contents of
+        Right val -> return val
+        Left err -> throwE $ "Failed to parse lastRunState file: " ++ err
 
 -- | Checks if the given last run state is valid for the current server and user.
 isValidLastRunState :: ChatResources -> User -> LastRunState -> Bool
 isValidLastRunState cr me rs =
-     rs^.lrsHost   == cr^.crConn.cdHostnameL
-  && rs^.lrsPort   == cr^.crConn.cdPortL
-  && rs^.lrsUserId == me^.userIdL
+    and [ rs^.lrsHost   == cr^.crConn.cdHostnameL
+        , rs^.lrsPort   == cr^.crConn.cdPortL
+        , rs^.lrsUserId == me^.userIdL
+        ]
