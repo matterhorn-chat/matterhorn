e08c8619c877272f83f473d2a30a424ba33499cd
Jonathan Daugherty <cygnus@foobox.com>
2022-05-06 17:16:49 -0700

    Improve thread window header

--- src/Matterhorn/Draw/ThreadWindow.hs
+++ src/Matterhorn/Draw/ThreadWindow.hs
@@ -9,11 +9,13 @@ import Matterhorn.Prelude
 import Brick
 import Brick.Widgets.Border
 import Lens.Micro.Platform (Lens', _Just, singular, SimpleGetter)
-import Network.Mattermost.Types (TeamId)
+import Network.Mattermost.Types (TeamId, Type(..))
 
 import Matterhorn.Types
 import Matterhorn.Draw.Main
 import Matterhorn.Draw.Autocomplete
+import Matterhorn.Draw.RichText
+import Matterhorn.Draw.Util
 
 drawThreadWindowLayers :: ChatState -> TeamId -> [Widget Name]
 drawThreadWindowLayers st tId =
@@ -35,10 +37,21 @@ drawThreadWindow st tId =
         hs = getHighlightSet st tId
         inMsgSelect = mode == ThreadWindowMessageSelect
         mode = st^.csTeam(tId).tsMode
+        cId = st^.ti.threadParentChannelId
+
+        title = case st^?csChannel(cId) of
+            Nothing -> "Thread"
+            Just chan ->
+                let prefix = case chan^.ccInfo.cdType of
+                        Group -> "Thread with "
+                        Direct -> "Thread with "
+                        _ -> "Thread in "
+                in prefix <> mkChannelName st (chan^.ccInfo)
 
         -- TODO: "Thread from ~<channel>" or "Thread with @<user>[, @<user>[, ...]]"
         -- depending on whether it's a DM/group/public thread or not
-        header = txt "Conversation with <TODO FIXME>"
+        header = vLimit 1 $ renderText' Nothing "" hs Nothing title <+> fill ' '
+
         body = header <=> hBorder <=> messageUI
         messageUI = drawMessageInterface st hs
                             (ThreadWindowMessages tId)
