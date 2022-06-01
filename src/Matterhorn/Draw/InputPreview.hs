0261ee8b479e64401d8117b420df4b688d188fd0
Jonathan Daugherty <cygnus@foobox.com>
2022-06-01 16:11:16 -0700

    Draw.Main: move functions related to input preview rendering to Draw.InputPreview

---
+++ src/Matterhorn/Draw/InputPreview.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE RankNTypes #-}
+module Matterhorn.Draw.InputPreview
+  ( inputPreview
+  )
+where
+
+import           Prelude ()
+import           Matterhorn.Prelude
+
+import           Brick
+import           Brick.Widgets.Border
+import           Brick.Widgets.Edit
+import           Control.Arrow ( (>>>) )
+import qualified Data.Text as T
+import           Data.Text.Zipper ( insertChar, getText, gotoEOL )
+import           Data.Time.Calendar ( fromGregorian )
+import           Data.Time.Clock ( UTCTime(..) )
+import qualified Graphics.Vty as Vty
+import           Lens.Micro.Platform ( SimpleGetter, to )
+
+import           Network.Mattermost.Types ( ServerTime(..), UserId, TeamId
+                                          )
+
+import           Matterhorn.Constants
+import           Matterhorn.Draw.Messages
+import           Matterhorn.Draw.RichText
+import           Matterhorn.Themes
+import           Matterhorn.Types
+import           Matterhorn.Types.RichText ( parseMarkdown, TeamBaseURL )
+
+
+inputPreview :: ChatState
+             -> SimpleGetter ChatState (EditState Name)
+             -> TeamId
+             -> Name
+             -> HighlightSet
+             -> Widget Name
+inputPreview st editWhich tId vpName hs
+    | not $ st^.csResources.crConfiguration.configShowMessagePreviewL = emptyWidget
+    | otherwise = thePreview
+    where
+    uId = myUserId st
+    -- Insert a cursor sentinel into the input text just before
+    -- rendering the preview. We use the inserted sentinel (which is
+    -- not rendered) to get brick to ensure that the line the cursor is
+    -- on is visible in the preview viewport. We put the sentinel at
+    -- the *end* of the line because it will still influence markdown
+    -- parsing and can create undesirable/confusing churn in the
+    -- rendering while the cursor moves around. If the cursor is at the
+    -- end of whatever line the user is editing, that is very unlikely
+    -- to be a problem.
+    curContents = getText $ (gotoEOL >>> insertChar cursorSentinel) $
+                  st^.editWhich.esEditor.editContentsL
+    curStr = T.intercalate "\n" curContents
+    overrideTy = case st^.editWhich.esEditMode of
+        Editing _ ty -> Just ty
+        _ -> Nothing
+    baseUrl = serverBaseUrl st tId
+    previewMsg = previewFromInput baseUrl overrideTy uId curStr
+    thePreview = let noPreview = str "(No preview)"
+                     msgPreview = case previewMsg of
+                       Nothing -> noPreview
+                       Just pm -> if T.null curStr
+                                  then noPreview
+                                  else prview pm $ getParentMessage st pm
+                     prview m p = renderMessage MessageData
+                                  { mdMessage           = m
+                                  , mdUserName          = m^.mUser.to (printableNameForUserRef st)
+                                  , mdParentMessage     = p
+                                  , mdParentUserName    = p >>= (^.mUser.to (printableNameForUserRef st))
+                                  , mdHighlightSet      = hs
+                                  , mdEditThreshold     = Nothing
+                                  , mdShowOlderEdits    = False
+                                  , mdRenderReplyParent = True
+                                  , mdRenderReplyIndent = True
+                                  , mdIndentBlocks      = True
+                                  , mdThreadState       = NoThread
+                                  , mdShowReactions     = True
+                                  , mdMessageWidthLimit = Nothing
+                                  , mdMyUsername        = myUsername st
+                                  , mdMyUserId          = myUserId st
+                                  , mdWrapNonhighlightedCodeBlocks = True
+                                  , mdTruncateVerbatimBlocks = Nothing
+                                  , mdClickableNameTag  = MessagePreviewViewport tId
+                                  }
+                 in (maybePreviewViewport vpName msgPreview) <=>
+                    hBorderWithLabel (withDefAttr clientEmphAttr $ str "[Preview ↑]")
+
+previewFromInput :: TeamBaseURL -> Maybe MessageType -> UserId -> Text -> Maybe Message
+previewFromInput _ _ _ s | s == T.singleton cursorSentinel = Nothing
+previewFromInput baseUrl overrideTy uId s =
+    -- If it starts with a slash but not /me, this has no preview
+    -- representation
+    let isCommand = "/" `T.isPrefixOf` s
+        isEmoteCmd = "/me " `T.isPrefixOf` s
+        content = if isEmoteCmd
+                  then T.stripStart $ T.drop 3 s
+                  else s
+        msgTy = fromMaybe (if isEmoteCmd then CP Emote else CP NormalPost) overrideTy
+    in if isCommand && not isEmoteCmd
+       then Nothing
+       else Just $ Message { _mText          = parseMarkdown (Just baseUrl) content
+                           , _mMarkdownSource = content
+                           , _mUser          = UserI False uId
+                           , _mDate          = ServerTime $ UTCTime (fromGregorian 1970 1 1) 0
+                           -- The date is not used for preview
+                           -- rendering, but we need to provide one.
+                           -- Ideally we'd just use today's date, but
+                           -- the rendering function is pure so we
+                           -- can't.
+                           , _mType          = msgTy
+                           , _mPending       = False
+                           , _mDeleted       = False
+                           , _mAttachments   = mempty
+                           , _mInReplyToMsg  = NotAReply
+                           , _mMessageId     = Nothing
+                           , _mReactions     = mempty
+                           , _mOriginalPost  = Nothing
+                           , _mFlagged       = False
+                           , _mPinned        = False
+                           , _mChannelId     = Nothing
+                           }
+
+maybePreviewViewport :: Name -> Widget Name -> Widget Name
+maybePreviewViewport n w =
+    Widget Greedy Fixed $ do
+        result <- render w
+        case (Vty.imageHeight $ result^.imageL) > previewMaxHeight of
+            False -> return result
+            True ->
+                render $ vLimit previewMaxHeight $ viewport n Vertical $
+                         (resultToWidget result)
--- src/Matterhorn/Draw/Main.hs
+++ src/Matterhorn/Draw/Main.hs
@@ -16,7 +16,6 @@ import           Brick.Widgets.Border.Style
 import           Brick.Widgets.Center ( centerLayer, center )
 import           Brick.Widgets.List ( listElements )
 import           Brick.Widgets.Edit ( editContentsL, renderEditor, getEditContents )
-import           Control.Arrow ( (>>>) )
 import           Data.Char ( isSpace, isPunctuation )
 import qualified Data.Foldable as F
 import           Data.List ( intersperse )
@@ -24,14 +23,12 @@ import           Data.Maybe ( fromJust )
 import qualified Data.Sequence as Seq
 import qualified Data.Set as S
 import qualified Data.Text as T
-import           Data.Text.Zipper ( cursorPosition, insertChar, getText, gotoEOL )
-import           Data.Time.Calendar ( fromGregorian )
+import           Data.Text.Zipper ( cursorPosition )
 import           Data.Time.Clock ( UTCTime(..) )
-import qualified Graphics.Vty as Vty
 import           Lens.Micro.Platform ( (.~), (^?!), to, view, Lens', Traversal', SimpleGetter )
 
 import           Network.Mattermost.Types ( ChannelId, Type(Direct, Private, Group)
-                                          , ServerTime(..), UserId, TeamId, teamDisplayName
+                                          , ServerTime(..), TeamId, teamDisplayName
                                           , teamId
                                           )
 
@@ -40,6 +37,7 @@ import           Matterhorn.Constants
 import           Matterhorn.Draw.ChannelList ( renderChannelList )
 import           Matterhorn.Draw.Messages
 import           Matterhorn.Draw.Autocomplete
+import           Matterhorn.Draw.InputPreview
 import           Matterhorn.Draw.Util
 import           Matterhorn.Draw.RichText
 import           Matterhorn.Events.Keybindings
@@ -50,7 +48,6 @@ import           Matterhorn.TimeUtils ( justAfter, justBefore )
 import           Matterhorn.Types
 import           Matterhorn.Types.Common ( sanitizeUserText )
 import           Matterhorn.Types.DirectionalSeq ( emptyDirSeq )
-import           Matterhorn.Types.RichText ( parseMarkdown, TeamBaseURL )
 import           Matterhorn.Types.KeyEvents
 import qualified Matterhorn.Zipper as Z
 
@@ -179,41 +176,6 @@ teamList st =
                         ]
                  , hBorder
                  ]
-
-previewFromInput :: TeamBaseURL -> Maybe MessageType -> UserId -> Text -> Maybe Message
-previewFromInput _ _ _ s | s == T.singleton cursorSentinel = Nothing
-previewFromInput baseUrl overrideTy uId s =
-    -- If it starts with a slash but not /me, this has no preview
-    -- representation
-    let isCommand = "/" `T.isPrefixOf` s
-        isEmoteCmd = "/me " `T.isPrefixOf` s
-        content = if isEmoteCmd
-                  then T.stripStart $ T.drop 3 s
-                  else s
-        msgTy = fromMaybe (if isEmoteCmd then CP Emote else CP NormalPost) overrideTy
-    in if isCommand && not isEmoteCmd
-       then Nothing
-       else Just $ Message { _mText          = parseMarkdown (Just baseUrl) content
-                           , _mMarkdownSource = content
-                           , _mUser          = UserI False uId
-                           , _mDate          = ServerTime $ UTCTime (fromGregorian 1970 1 1) 0
-                           -- The date is not used for preview
-                           -- rendering, but we need to provide one.
-                           -- Ideally we'd just use today's date, but
-                           -- the rendering function is pure so we
-                           -- can't.
-                           , _mType          = msgTy
-                           , _mPending       = False
-                           , _mDeleted       = False
-                           , _mAttachments   = mempty
-                           , _mInReplyToMsg  = NotAReply
-                           , _mMessageId     = Nothing
-                           , _mReactions     = mempty
-                           , _mOriginalPost  = Nothing
-                           , _mFlagged       = False
-                           , _mPinned        = False
-                           , _mChannelId     = Nothing
-                           }
 
 -- | Tokens in spell check highlighting.
 data Token =
@@ -711,73 +673,6 @@ messageSelectBottomBar st tId which =
                     , txt "]"
                     , hBorder
                     ]
-
-maybePreviewViewport :: Name -> Widget Name -> Widget Name
-maybePreviewViewport n w =
-    Widget Greedy Fixed $ do
-        result <- render w
-        case (Vty.imageHeight $ result^.imageL) > previewMaxHeight of
-            False -> return result
-            True ->
-                render $ vLimit previewMaxHeight $ viewport n Vertical $
-                         (resultToWidget result)
-
-inputPreview :: ChatState
-             -> SimpleGetter ChatState (EditState Name)
-             -> TeamId
-             -> Name
-             -> HighlightSet
-             -> Widget Name
-inputPreview st editWhich tId vpName hs
-    | not $ st^.csResources.crConfiguration.configShowMessagePreviewL = emptyWidget
-    | otherwise = thePreview
-    where
-    uId = myUserId st
-    -- Insert a cursor sentinel into the input text just before
-    -- rendering the preview. We use the inserted sentinel (which is
-    -- not rendered) to get brick to ensure that the line the cursor is
-    -- on is visible in the preview viewport. We put the sentinel at
-    -- the *end* of the line because it will still influence markdown
-    -- parsing and can create undesirable/confusing churn in the
-    -- rendering while the cursor moves around. If the cursor is at the
-    -- end of whatever line the user is editing, that is very unlikely
-    -- to be a problem.
-    curContents = getText $ (gotoEOL >>> insertChar cursorSentinel) $
-                  st^.editWhich.esEditor.editContentsL
-    curStr = T.intercalate "\n" curContents
-    overrideTy = case st^.editWhich.esEditMode of
-        Editing _ ty -> Just ty
-        _ -> Nothing
-    baseUrl = serverBaseUrl st tId
-    previewMsg = previewFromInput baseUrl overrideTy uId curStr
-    thePreview = let noPreview = str "(No preview)"
-                     msgPreview = case previewMsg of
-                       Nothing -> noPreview
-                       Just pm -> if T.null curStr
-                                  then noPreview
-                                  else prview pm $ getParentMessage st pm
-                     prview m p = renderMessage MessageData
-                                  { mdMessage           = m
-                                  , mdUserName          = m^.mUser.to (printableNameForUserRef st)
-                                  , mdParentMessage     = p
-                                  , mdParentUserName    = p >>= (^.mUser.to (printableNameForUserRef st))
-                                  , mdHighlightSet      = hs
-                                  , mdEditThreshold     = Nothing
-                                  , mdShowOlderEdits    = False
-                                  , mdRenderReplyParent = True
-                                  , mdRenderReplyIndent = True
-                                  , mdIndentBlocks      = True
-                                  , mdThreadState       = NoThread
-                                  , mdShowReactions     = True
-                                  , mdMessageWidthLimit = Nothing
-                                  , mdMyUsername        = myUsername st
-                                  , mdMyUserId          = myUserId st
-                                  , mdWrapNonhighlightedCodeBlocks = True
-                                  , mdTruncateVerbatimBlocks = Nothing
-                                  , mdClickableNameTag  = MessagePreviewViewport tId
-                                  }
-                 in (maybePreviewViewport vpName msgPreview) <=>
-                    hBorderWithLabel (withDefAttr clientEmphAttr $ str "[Preview ↑]")
 
 drawDeleteMessageConfirm :: Widget Name
 drawDeleteMessageConfirm =
