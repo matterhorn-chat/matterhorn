7adda6adf585afa247f24ad01238c8806cb8ea04
Jonathan Daugherty <cygnus@foobox.com>
2022-09-26 14:38:50 -0700

    drawButton: make buttons clickable

--- src/Matterhorn/Draw/Buttons.hs
+++ src/Matterhorn/Draw/Buttons.hs
@@ -18,12 +18,13 @@ import           Matterhorn.Themes
 buttonWidth :: Int
 buttonWidth = 10
 
-drawButton :: (Eq n) => FocusRing n -> n -> T.Text -> Widget n
+drawButton :: (Eq n, Ord n) => FocusRing n -> n -> T.Text -> Widget n
 drawButton f n label =
     let attr = if focusGetCurrent f == Just n
                then buttonFocusedAttr
                else buttonAttr
     in withDefAttr attr $
+       clickable n $
        hLimit buttonWidth $
        hCenter $
        txt label
