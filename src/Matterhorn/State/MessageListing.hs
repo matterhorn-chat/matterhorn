20a40cb405dbe79e619b3aa0deff311a2007275e
Jonathan Daugherty <cygnus@foobox.com>
2026-04-23 20:53:20 -0700

    State.MessageListing: remove unused import

--- src/Matterhorn/State/MessageListing.hs
+++ src/Matterhorn/State/MessageListing.hs
@@ -28,7 +28,6 @@ import           Matterhorn.Prelude
 import           Brick ( invalidateCache )
 import           Control.Monad ( replicateM_ )
 import           Data.Maybe ( fromJust )
-import qualified Data.Text as T
 import           Lens.Micro.Platform
 
 import           Network.Mattermost.Types
